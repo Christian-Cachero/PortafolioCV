c3636ca9bdc69404df8ca3f1826bd4b8d8408118
Christian Cachero <Ch_canchero@live.com.ar>
2022-08-19 18:17:14 -0300

    se creo esta wea para poner mis proyectos de SpringBoot

--- Chat/index.js
+++ Chat/index.js
@@ -1,22 +1,32 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http");
+const http = require('http');
+const { emit } = require('process');
 const server = http.createServer(app);
-const { Server, Socket } = require("socket.io");
+const { Server } = require("socket.io");
 const io = new Server(server);
 
+//lado del servidor?
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
-
-io.on("connection", (Socket) => {
-    Socket.on("chat message", (msg) => {
+ 
+ 
+io.on("connection", (socket) => {
+    socket.on("chat message", (msg) => {
         io.emit("chat message", msg);
-    });
-});
+      });
+  });
 
 
-  server.listen(3000, () => {
+  //io.on("connection", (socket) => {
+  //  socket.emit("hello", "world");
+  //});
+
+
+  
+
+  server.listen(3001, () => {
     console.log("escuchando en el puerto *:3000");
 });
 
